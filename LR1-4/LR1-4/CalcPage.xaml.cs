6052f6f065ba78e5c424ce2fc6bb01f7b4aec086
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-02-13 23:25:03 +0300

    Added all finctional for binary expressions

--- LR1-4/LR1-4/CalcPage.xaml.cs
+++ LR1-4/LR1-4/CalcPage.xaml.cs
@@ -1,11 +1,13 @@
-using System.Diagnostics;
+﻿using System.Diagnostics;
+using System.Linq.Expressions;
 using System.Text;
 
 namespace LR1_4;
 
 public partial class CalcPage : ContentPage
 {
-	private StringBuilder stringBuilder = new StringBuilder("0");
+	private StringBuilder insertion = new StringBuilder("0");
+    private BinaryExpression expression = new BinaryExpression();
 	private bool isFractional = false;
     private bool isNegative = false;
 
@@ -16,87 +18,160 @@ public partial class CalcPage : ContentPage
 
 	private void OnNumButtonClicked(object sender, EventArgs e)
 	{
-        if (stringBuilder.Length >= 11) return;
-        if (stringBuilder.ToString() == "0") stringBuilder.Clear();
+        if (insertion.Length >= 11) return;
+        if (insertion.ToString() == "0") insertion.Clear();
         Button button = (Button)sender;
 		switch (button.Text)
 		{
 			case "0":
-				stringBuilder.Append("0");
+				insertion.Append("0");
 				break;
 			case "1":
-				stringBuilder.Append("1");
+				insertion.Append("1");
 				break;
             case "2":
-                stringBuilder.Append("2");
+                insertion.Append("2");
                 break;
             case "3":
-                stringBuilder.Append("3");
+                insertion.Append("3");
                 break;
             case "4":
-                stringBuilder.Append("4");
+                insertion.Append("4");
                 break;
             case "5":
-                stringBuilder.Append("5");
+                insertion.Append("5");
                 break;
             case "6":
-                stringBuilder.Append("6");
+                insertion.Append("6");
                 break;
             case "7":
-                stringBuilder.Append("7");
+                insertion.Append("7");
                 break;
             case "8":
-                stringBuilder.Append("8");
+                insertion.Append("8");
                 break;
             case "9":
-                stringBuilder.Append("9");
+                insertion.Append("9");
                 break;
         }
-        EnterLabel.Text = stringBuilder.ToString();
+        EnterLabel.Text = insertion.ToString();
 	}
 
     private void OnDelButtonClicked(object sender, EventArgs e)
     {
-        if (stringBuilder.ToString() == "0") return;
-        if (stringBuilder.Length > 0)
+        if (insertion.ToString() == "0") return;
+        if (insertion.Length > 0)
         {            
-            if (stringBuilder[stringBuilder.Length - 1] == ',') isFractional = false;
-            stringBuilder.Length--;
-            if (stringBuilder.Length == 0) stringBuilder.Append("0");
-            if (isNegative && stringBuilder.Length == 1)
+            if (insertion[insertion.Length - 1] == ',') isFractional = false;
+            insertion.Length--;
+            if (insertion.Length == 0) insertion.Append("0");
+            if (isNegative && insertion.Length == 1)
             {
-                stringBuilder.Clear();
-                stringBuilder.Append("0");
+                insertion.Clear();
+                insertion.Append("0");
                 isNegative = false;
             }
-            EnterLabel.Text = stringBuilder.ToString();
+            EnterLabel.Text = insertion.ToString();
         }
     }
 
     private void OnSignButtonClicked(object sender, EventArgs e)
     {
-        if (stringBuilder.ToString() == "0" || stringBuilder.Length == 0) return;
+        if (insertion.ToString() == "0" || insertion.Length == 0) return;
         if (isNegative)
         {
-            stringBuilder.Remove(0, 1);
+            insertion.Remove(0, 1);
             isNegative = false;
         }
         else
         {
-            stringBuilder.Insert(0, "-");
+            insertion.Insert(0, "-");
             isNegative = true;
         }
-        EnterLabel.Text = stringBuilder.ToString();
+        EnterLabel.Text = insertion.ToString();
     }
 
     private void OnFractionalButtonClicked(object sender, EventArgs e)
     {
-        if (stringBuilder.ToString() == "0") return;
+        if (insertion.ToString() == "0") return;
         if (!isFractional)
         {
-            stringBuilder.Append(",");
+            insertion.Append(",");
             isFractional = true;
         }
-        EnterLabel.Text = stringBuilder.ToString();
+        EnterLabel.Text = insertion.ToString();
+    }
+
+    private void OnBinaryExpressionButtonClicked(object sender, EventArgs e)
+    {
+        Button button = (Button)sender;
+        expression.Operand1 = insertion.ToString();
+        insertion.Clear();
+        insertion.Append('0');
+        isNegative = false;
+        isFractional = false;
+        switch (button.Text)
+        {
+            case "+":
+                expression.OperatorString = "+";
+                expression.Operator = (double a, double b) => a + b;
+                break;
+            case "-":
+                expression.OperatorString = "-";
+                expression.Operator = (double a, double b) => a - b;
+                break;
+            case "*":
+                expression.OperatorString = "*";
+                expression.Operator = (double a, double b) => a * b;
+                break;
+            case "÷":
+                expression.OperatorString = "÷";
+                expression.Operator = (double a, double b) => a / b;
+                break;
+            case "mod":
+                expression.OperatorString = "mod";
+                expression.Operator = (double a, double b) => a % b;
+                break;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append(expression.Operand1);
+        stringBuilder.Append(" ");
+        stringBuilder.Append(expression.OperatorString);
+        ExpressionLabel.Text = stringBuilder.ToString();
+    }
+
+    private void OnEqualsButtonClicked(object sender, EventArgs e)
+    {
+        expression.Operand2 = insertion.ToString();
+        double answ = 0;
+        try{
+            answ = expression.Evaluate();
+        }
+        catch{
+            return;
+        }
+
+        expression.Operand1 = expression.Operand2;
+        expression.Operand2 = Convert.ToString(answ);
+        insertion.Clear();
+        insertion.Append(expression.Operand2);
+
+        if (answ - Math.Floor(answ) == 0) isFractional = false;
+        else isFractional = true;
+
+        if (answ >= 0) isNegative = false;
+        else isNegative = true;
+
+        EnterLabel.Text = insertion.ToString();
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.Append(expression.Operand1);
+        stringBuilder.Append(" ");
+        stringBuilder.Append(expression.OperatorString);
+        stringBuilder.Append(" ");
+        stringBuilder.Append(expression.Operand2);
+        stringBuilder.Append(" ");
+        stringBuilder.Append("=");
+        ExpressionLabel.Text = stringBuilder.ToString();
     }
 }
