5d28f463f56a866e61b6b37adb19080e2bf17c81
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-02-18 19:39:29 +0300

    Added working progress bar

--- Lab1Pavlovich253505/Lab1Pavlovich253505/ProgressBar.xaml.cs
+++ Lab1Pavlovich253505/Lab1Pavlovich253505/ProgressBar.xaml.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+
 namespace Lab1Pavlovich253505;
 
 public partial class ProgressBar : ContentPage
@@ -6,4 +8,31 @@ public partial class ProgressBar : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async Task<double> calculate()
+    {
+        double n = 1e8;
+        double h = 1 / n;
+        double result = 0;
+        double perCounter = 0;
+        for (int i = 0; i < n; i++)
+        {
+            double per = i / n;
+            result += Math.Sin(h / 2 + i * h);
+            if (perCounter < per)
+            {
+                perCounter += 0.01;
+                ProgressPercentLabel.Text = $"{Math.Round(perCounter*100,0)}%";
+                await CalculationProgressBar.ProgressTo(perCounter, 10, Easing.Linear);
+            }
+        }
+        result *= h;
+        return result;
+    }
+
+    private async void OnStartButtonClicked(object sender, EventArgs e)
+    {
+        double answ = await calculate();
+        StatusLabel.Text = answ.ToString();
+    }
 }
