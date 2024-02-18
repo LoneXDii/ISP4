243fcf0bb3f2bcbb5202f82a0c20ea21b84d9cc2
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-02-18 19:59:44 +0300

    Ended Lab2

--- Lab1Pavlovich253505/Lab1Pavlovich253505/ProgressBar.xaml.cs
+++ Lab1Pavlovich253505/Lab1Pavlovich253505/ProgressBar.xaml.cs
@@ -4,12 +4,14 @@ namespace Lab1Pavlovich253505;
 
 public partial class ProgressBar : ContentPage
 {
+    private static CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+    private CancellationToken cancellationToken = cancellationTokenSource.Token;
 	public ProgressBar()
 	{
 		InitializeComponent();
 	}
 
-    private async Task<double> calculate()
+    private async Task calculate()
     {
         double n = 1e8;
         double h = 1 / n;
@@ -19,20 +21,34 @@ public partial class ProgressBar : ContentPage
         {
             double per = i / n;
             result += Math.Sin(h / 2 + i * h);
+            if (cancellationToken.IsCancellationRequested)
+            {
+                return;
+            }
             if (perCounter < per)
             {
-                perCounter += 0.01;
+                perCounter += 0.001;
                 ProgressPercentLabel.Text = $"{Math.Round(perCounter*100,0)}%";
-                await CalculationProgressBar.ProgressTo(perCounter, 10, Easing.Linear);
+                await CalculationProgressBar.ProgressTo(perCounter, 5, Easing.Linear);
             }
         }
         result *= h;
-        return result;
+        StatusLabel.Text = result.ToString();
     }
 
     private async void OnStartButtonClicked(object sender, EventArgs e)
     {
-        double answ = await calculate();
-        StatusLabel.Text = answ.ToString();
+        StartButton.IsEnabled = false;
+        StatusLabel.Text = "Вычисление";
+        cancellationTokenSource = new CancellationTokenSource();
+        cancellationToken = cancellationTokenSource.Token;
+        await calculate();
+        StartButton.IsEnabled = true;
+    }
+
+    private void OnCancellButtonClicked(object sender, EventArgs e)
+    {
+        StatusLabel.Text = "Задание отменено";
+        cancellationTokenSource.Cancel();
     }
 }
