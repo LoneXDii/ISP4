8b29441078bf1bea935a15771c5ca18c17132231
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-02-24 20:33:00 +0300

    Completed Lab4

--- Lab1Pavlovich253505/Lab1Pavlovich253505/Pages/ConverterPage.xaml.cs
+++ Lab1Pavlovich253505/Lab1Pavlovich253505/Pages/ConverterPage.xaml.cs
@@ -7,11 +7,14 @@ public partial class ConverterPage : ContentPage
 {
 	private IRateService rateService;
     private decimal? officialRate = 0;
+    private int scale = 0;
+    private bool isInEntryEntered = false;
     public ConverterPage(IRateService service)
     {
         InitializeComponent();
         rateService = service;
         RateDatePicker.MaximumDate = DateTime.Today;
+        isInEntryEntered = true;
     }
 
 
@@ -25,18 +28,76 @@ public partial class ConverterPage : ContentPage
             if(rate.Cur_Abbreviation == currency)
             {
                 officialRate = rate.Cur_OfficialRate;
+                scale = rate.Cur_Scale;
                 break;
             }
         }
+
+        isInEntryEntered = false;
+        decimal? sum;
+        try
+        {
+            sum = Convert.ToDecimal(BYNEntry.Text);
+            sum /= officialRate / scale;
+            sum = Math.Round(sum.Value, 4);
+            CurrencyEntry.Text = sum.ToString();
+            isInEntryEntered = true;
+        }
+        catch
+        {
+            isInEntryEntered = true;
+            return;
+        }
+        
 	}
 
     private void OnBYNEntryChanged(object sender, EventArgs e)
 	{
-
-	}
+        if (!isInEntryEntered) return;
+        if (officialRate == 0 || BYNEntry.Text == "")
+        {
+            BYNEntry.Text = "";
+            return;
+        }
+        isInEntryEntered = false;
+        decimal? sum;
+        try
+        {
+            sum = Convert.ToDecimal(BYNEntry.Text);
+            sum /= officialRate / scale;
+            sum = Math.Round(sum.Value, 4);
+            CurrencyEntry.Text = sum.ToString();
+            isInEntryEntered = true;
+        }
+        catch
+        {
+            isInEntryEntered = true;
+            return;
+        }
+    }
 
 	private void OnCurrencyEntryChanged(object sender, EventArgs e)
 	{
-
-	}
+        if (!isInEntryEntered) return;
+        if (officialRate == 0 || CurrencyEntry.Text == "")
+        {
+            CurrencyEntry.Text = "";
+            return;
+        }
+        isInEntryEntered = false;
+        decimal? sum;
+        try
+        {
+            sum = Convert.ToDecimal(CurrencyEntry.Text);
+            sum *= officialRate / scale;
+            sum = Math.Round(sum.Value, 4);
+            BYNEntry.Text = sum.ToString();
+            isInEntryEntered = true;
+        }
+        catch
+        {
+            isInEntryEntered = true;
+            return;
+        }
+    }
 }
