74467fbb25b475c645cbadab268b767f5351d4a2
Vlad Pavlovich <vlad.pavlovich.04@mail.ru>
2024-02-24 16:47:38 +0300

    Temp

--- Lab1Pavlovich253505/Lab1Pavlovich253505/Pages/HotelRooms.xaml.cs
+++ Lab1Pavlovich253505/Lab1Pavlovich253505/Pages/HotelRooms.xaml.cs
@@ -22,9 +22,9 @@ public partial class HotelRooms : ContentPage
 	private void OnSelectedIndexChanged(object sender, EventArgs e)
 	{
 		var picker = (Picker)sender;
-		int index = picker.SelectedIndex;
-		if(index != -1)
+		if (picker.SelectedIndex != -1)
 		{
+			int index = ((HotelRoomCategory)picker.SelectedItem).Id;
 			var categoryFeatures = dbService.GetCategoryFeatures(index + 1).ToList();
 			CategoryFeaturesView.ItemsSource = categoryFeatures;
 		}
